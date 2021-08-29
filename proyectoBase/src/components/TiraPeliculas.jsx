1c92b503d9e8ffce10e00dc5b4406d49116c57e6
florcadelli <florcadelli@gmail.com>
2021-08-29 14:58:46 -0300

    boton funcionando

--- proyectoBase/src/components/TiraPeliculas.jsx
+++ proyectoBase/src/components/TiraPeliculas.jsx
@@ -1,27 +1,13 @@
 import Pelicula from "./Pelicula";
 import "../styles/peliculas.css";
-import React, { Component } from "react";
+import React, { Component } from "react"
 
-// function TiraPeliculas(props) {
-class TiraPeliculas extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      peliculas: props.peliculas.peliculas.peliculas1
-    }; 
-   
-    this.cambiarPeliculas.bind(this);
-  }
 
-   cambiarPeliculas(){
-    this.setState({peliculas: this.props.peliculas.peliculas.peliculas2})
-  }
-
-  render() {
-    return (
-      <div className="tira-de-peliculas">
+function TiraPeliculas(props) {
+  return (
+    <div className="tira-de-peliculas">
       
-        {this.state.peliculas.map((pelicula, i) => (
+        {props.info.map((pelicula, i) => (
           <Pelicula
             key={pelicula + 1}
             titulo={pelicula.titulo}
@@ -30,11 +16,14 @@ class TiraPeliculas extends Component {
           />
         ))}
 
-        <p>tira de cambiarPeliculas</p>
+         <p>tira de cambiarPeliculas</p>
 
-        <button onClick={this.cambiarPeliculas}>Ver mas peliculas</button>
-      </div>
-    );
-  }
+       </div>
+  
+  );
 }
 export default TiraPeliculas;
+
+
+
+
--- proyectoBase/src/container/App.js
+++ proyectoBase/src/container/App.js
@@ -2,78 +2,76 @@ import "../styles/App.css";
 import TiraPeliculas from "../components/TiraPeliculas";
 import NavBar from "../components/NavBar";
 import Boton from "../components/Boton";
-
-
 import React, { Component } from 'react'
 
 //fuera del componente de clase
 let textoHeader = "Soy el header";
 let enlaces = ["home", "Productos", "Servicios", "Sucursales", "Contacto"]
 let textoFooter = "Soy el footer";
-/* ------------------------------- peliculas1 ------------------------------- */
-let peliculas = {
-  peliculas1 : [
-    {
-      titulo: "Pelicula1",
-      rating: "8.0",
-      generos: ["drama", "accion"]
-    },
-    {
-      titulo: "Pelicula2",
-      rating: "7.0",
-      generos: ["comedia", "accion"]
-    },
-    {
-      titulo: "Pelicula3",
-      rating: "6.0",
-      generos: ["terror"]
-    },
-    {
-      titulo: "Pelicula4",
-      rating: "5.0",
-      generos: ["terror"]
-    }
-  ],
-   peliculas2 : [
-    {
-      titulo: "Pelicula5",
-      rating: "8.0",
-      generos: ["drama", "accion"]
-    },
-    {
-      titulo: "Pelicula6",
-      rating: "7.0",
-      generos: ["comedia", "accion"]
-    },
-    {
-      titulo: "Pelicula7",
-      rating: "6.0",
-      generos: ["terror"]
-    },
-    {
-      titulo: "Pelicula8",
-      rating: "5.0",
-      generos: ["terror"]
-    }
-  ]
-
-}
-
-
+let peliculas1 = [
+  {
+    titulo: "Pelicula1",
+    rating: "8.0",
+    generos: ["drama", "accion"]
+  },
+  {
+    titulo: "Pelicula2",
+    rating: "7.0",
+    generos: ["comedia", "accion"]
+  },
+  {
+    titulo: "Pelicula3",
+    rating: "6.0",
+    generos: ["terror"]
+  },
+  {
+    titulo: "Pelicula4",
+    rating: "5.0",
+    generos: ["terror"]
+  }
+]
+let peliculas2 = [
+  {
+    titulo: "Pelicula5",
+    rating: "8.0",
+    generos: ["drama", "accion"]
+  },
+  {
+    titulo: "Pelicula6",
+    rating: "7.0",
+    generos: ["comedia", "accion"]
+  },
+  {
+    titulo: "Pelicula7",
+    rating: "6.0",
+    generos: ["terror"]
+  },
+  {
+    titulo: "Pelicula8",
+    rating: "5.0",
+    generos: ["terror"]
+  }
+]
 
 
 
 class App extends Component {
-/* ------------------------------- constructor ------------------------------ */
-constructor(){
-  super();
-  this.state = {
-    peliculas :{peliculas}  }
-} 
-/* -------------------------------- funciones ------------------------------- */
+  constructor (){
+    super();
+    this.state = { 
+      peliculas : peliculas1}
+  }
 
+cambiarPeliculas() {
+  if(this.state.peliculas == peliculas1){
+    this.setState ({peliculas : peliculas2})
+  }else{
+    this.setState ({peliculas : peliculas1})
+  }
+  
+  
+}
 
-/* --------------------------------- render --------------------------------- */
 render() {
  
    return (
@@ -82,10 +80,8 @@ render() {
                <NavBar enlaces = {enlaces} />
              </header>
              <main>
-                 <TiraPeliculas peliculas={this.state.peliculas} />
-                 {/* <Boton peliculas={this.state.peliculas} /> */}
-                 {/* <Boton funcion={this.cambiarPeliculas} />    */}
-                 {/* <button onClick={ () => this.cambiarPeliculas()}>Ver mas peliculas</button>     */}
+             <TiraPeliculas  info = {this.state.peliculas}/>
+             <button onClick={ ()=> this.cambiarPeliculas()}> Cambiar Peliculas</button>
              </main>
              <footer><p>{textoFooter}</p></footer> 
          </div>
